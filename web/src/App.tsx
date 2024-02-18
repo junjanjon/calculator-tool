af3d98d8c5bd9626c6aab80fbb514357f2e9aeee
hiroi_junki <junki.hiroi@drecom.co.jp>
2024-02-18 15:59:14 +0900

    Add シンプル電卓機能追加

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,33 +1,78 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Input from '@mui/joy/Input';
+import Grid from '@mui/joy/Grid';
+import { Button } from "@mui/joy";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [formula, setFormula] = useState("")
+
+  const onClick = (value: string) => {
+    setFormula(formula + value)
+  }
+
+  const onClickEqual = () => {
+    try {
+      const result = eval(formula);
+      setFormula(result);
+    }
+    catch (e) {
+      setFormula("error");
+    }
+  };
+
+  const onClickClear = () => {
+    setFormula("");
+  }
+
+  const buttons = [
+    {value: "AC", onClick: () => onClickClear()},
+    {value: "", onClick: () => onClickClear()},
+    {value: "", onClick: () => onClickClear()},
+    {value: "/", onClick: () => onClick("/")},
+    {value: "7", onClick: () => onClick("7")},
+    {value: "8", onClick: () => onClick("8")},
+    {value: "9", onClick: () => onClick("9")},
+    {value: "*", onClick: () => onClick("*")},
+    {value: "4", onClick: () => onClick("4")},
+    {value: "5", onClick: () => onClick("5")},
+    {value: "6", onClick: () => onClick("6")},
+    {value: "-", onClick: () => onClick("-")},
+    {value: "1", onClick: () => onClick("1")},
+    {value: "2", onClick: () => onClick("2")},
+    {value: "3", onClick: () => onClick("3")},
+    {value: "+", onClick: () => onClick("+")},
+    {value: "0", onClick: () => onClick("0")},
+    {value: ".", onClick: () => onClick(".")},
+    {value: <></>, onClick: () => onClickEqual()},
+    {value: "=", onClick: () => onClickEqual()},
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Grid
+        container
+        spacing={2}
+        columns={{ xs:4 }}>
+        <Grid xs={4}>
+          <Input
+            color="primary"
+            variant="solid"
+            size={"lg"}
+            value={formula}/>
+        </Grid>
+        {
+          buttons.map((button) => {
+            return (
+              <Grid xs={1}>
+                <Button
+                  onClick={button.onClick}
+                  size={"lg"}>{button.value}</Button>
+              </Grid>
+            )
+          })
+        }
+      </Grid>
     </>
   )
 }
